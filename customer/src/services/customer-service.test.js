ece11a02fbd969d80e5f18122ca695f18a4d793a
Mizanur Rahman <nickty.86@gmail.com>
2023-01-14 19:27:32 +0600

    123

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -3,7 +3,7 @@ describe("Customer Service", () => {
   //which function we are testing
 
   describe("SignIn", () => {
-    // which scenario we are testing
+    // which scenario we are testing 123
     test("validate user inputs", () => {});
 
     // just a title
